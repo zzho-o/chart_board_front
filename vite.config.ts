8750676d85596a1b5de0b1919c274f74b7966bf0
zzho0 <rkdwjsgh0@mxlab.kr>
2025-11-07 15:12:45 +0900

    feat:env

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  base: process.env.BASE_PATH || '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@pages': '/src/pages',
-      '@stores': '/src/stores',
-      '@hooks': '/src/hooks',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  console.log('✅ VITE_API_URL:', env.VITE_API_URL);
+  console.log('✅ BASE_PATH:', env.BASE_PATH);
+
+  return {
+    base: env.BASE_PATH || '/',
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
     },
-  },
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@assets': '/src/assets',
+        '@pages': '/src/pages',
+        '@stores': '/src/stores',
+        '@hooks': '/src/hooks',
+      },
+    },
+  };
 });
